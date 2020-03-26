8da7127e58ca87345b3408fd9e840178ba6c34b2
Felipe <felipesonegocabeca9@hotmail.com>
2020-03-26 18:06:17 -0300

    Front-end do app

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -7,22 +7,21 @@ module.exports = {
         const [count] = await connection('incidents')
         .count();
 
-        console.log(count);
 
         const incidents = await connection('incidents')
         .join('ongs', 'ongs.id', '=', 'incidents.ong_id')
         .limit(5)
         .offset((page - 1) * 5)
         .select(['incidents.*', 
-        'ongs.name', 
-        'ongs.email', 
-        'ongs.whatsapp', 
-        'ongs.city', 
-        'ongs.uf']);
+                 'ongs.name', 
+                 'ongs.email', 
+                 'ongs.whatsapp', 
+                 'ongs.city', 
+                 'ongs.uf']);
 
         response.header('X-Total-Count', count['count(*)']);
 
-        return response.json(incidents)
+        return response.json(incidents);
     },
 
     async create(request, response) {
